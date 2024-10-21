97acecc398460930c4c303c19c31bbee15ae5c99
Stiven <tu-nuevo-email@example.com>
2024-10-20 22:53:29 -0500

    seis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,13 +21,13 @@ export default function Home() {
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://github.com/Stiven073120/frontend"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Image
             aria-hidden
-            src="https://github.com/Stiven073120/frontend"
+            src="nextjs.org/icons/window.svg"
             alt="Window icon"
             width={16}
             height={16}
@@ -36,13 +36,13 @@ export default function Home() {
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://github.com/Stiven073120/backend"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Image
             aria-hidden
-            src="https://github.com/Stiven073120/backend"
+            src="nextjs.org/icons/window.svg"
             alt="Window icon"
             width={16}
             height={16}
